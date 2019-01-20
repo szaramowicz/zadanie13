ad568a2e08c9690e1e84ea17880893cf2f53756c
szaramowicz <44734120+szaramowicz@users.noreply.github.com>
2019-01-20 11:32:44 +0100

    Add files via upload

---
+++ zadanie12.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import aseegg as ag
+
+dane = pd.read_csv("sub-02_trial-03.csv", delimiter=',', engine='python')
+
+czProbkowania=200
+t = np.linspace (0, 38.01, czProbkowania*38.01)
+sygnal=dane['sygnal']
+plt.plot(t, sygnal)
+plt.xlabel("Czas [s]")
+plt.ylabel("Amplituda [-]")
+plt.show()
+
+przefiltrowany1 = ag.pasmowozaporowy(sygnal, czProbkowania, -0.115, -0.123)
+przefiltrowany2 = ag.pasmowoprzepustowy(przefiltrowany1, czProbkowania, -0.115, -0.110)
+
+plt.plot(t, przefiltrowany2)
+plt.show()
